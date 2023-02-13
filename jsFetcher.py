3e297fc535d85f0caaaa2b2698045c5e958fee90
ja1sh <jaisharmafps@gmail.com>
2023-02-13 17:54:23 +0530

    pushx02

--- jsFetcher.py
+++ jsFetcher.py
@@ -1,39 +1,45 @@
 import requests
 import os
 import json
-from tqdm import tqdm
 import argparse
+import concurrent.futures
+from fake_useragent import UserAgent
+import time
+import random
+
+
+ua = UserAgent(browsers=['edge', 'chrome', 'safari', 'ie', 'firefox'])
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="file containing list of endpoints")
+parser.add_argument("path", help="path to dump the js files")
 args = parser.parse_args()
+
+
+def get_response(endpoint):
+    headers = {
+    "User-Agent": ua.random,
+    "Accept": "text/html,application/xhtml+xml,application/xml",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Referer": "https://www.google.com/"
+    }
+    delay = random.uniform(0, 2)
+    time.sleep(delay)
+    response = requests.get(endpoint, headers=headers)
+    if response.status_code == 200:
+        filename = f"{endpoint.split('/')[-1]}"[:8] + '_' + f"{endpoint.split('/')[-1]}"[-8:] + ".js"
+        filepath = os.path.join(folder_name, filename)
+        with open(filepath, "w") as f:
+            f.write(response.content)
+    else:
+        print(f"{endpoint} returned {response.status_code} status code")
 
 with open(args.file) as file:
     endpoints = file.readlines()
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Referer": "https://www.google.com/"
-}
-
-folder_name = "js_endpoint_responses"
+folder_name = os.path.abspath(args.path)
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
-for endpoint in tqdm(endpoints):
-    endpoint = endpoint.strip()
-    response = requests.get(endpoint, headers=headers)
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            filename = f"{endpoint.split('/')[-1]}.json"
-            filepath = os.path.join(folder_name, filename)
-            with open(filepath, "w") as f:
-                json.dump(data, f)
-        except json.decoder.JSONDecodeError as e:
-            print(f"{endpoint} is not returning json data")
-    else:
-        print(f"{endpoint} returned {response.status_code} status code")
-
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(get_response, endpoints)
